6bda0d1b470c1f06ed903d47f8beab5ae16e0141
Georgina Gonzalez <ginaglezi2000@gmail.com>
2022-08-26 23:12:29 -0700

    Updated MLP.py to python3.10.6 keras2.9.0 tf2.9.1

--- MLP.py
+++ MLP.py
@@ -1,4 +1,9 @@
 '''
+UserWarning: "`binary_crossentropy` received `from_logits=True`, but the `output` argument was produced by a sigmoid or softmax activation and thus does not represent logits. Was this intended?"
+  return dispatch_target(*args, **kwargs)
+'''
+
+'''
 Created on Aug 9, 2016
 Keras Implementation of Multi-Layer Perceptron (GMF) recommender model in:
 He Xiangnan et al. Neural Collaborative Filtering. In WWW 2017.  
@@ -8,23 +13,29 @@ He Xiangnan et al. Neural Collaborative Filtering. In WWW 2017.
 
 import numpy as np
 
-import theano
-import theano.tensor as T
-import keras
-from keras import backend as K
-from keras import initializations
-from keras.regularizers import l2, activity_l2
-from keras.models import Sequential, Graph, Model
-from keras.layers.core import Dense, Lambda, Activation
-from keras.layers import Embedding, Input, Dense, merge, Reshape, Merge, Flatten, Dropout
-from keras.constraints import maxnorm
-from keras.optimizers import Adagrad, Adam, SGD, RMSprop
+import tensorflow
+import tensorflow.keras
+# from tensorflow.keras import backend as K
+from tensorflow.keras import initializers    # In Keras 1 it was called initializations
+from tensorflow.keras.regularizers import L2    # In Keras 1 it was called activity_l2
+# ImportError: cannot import name 'Graph' from 'tensorflow.keras.models'
+# from tensorflow.keras.models import Sequential, Graph, Model
+from tensorflow.keras.models import Model
+# ModuleNotFoundError: No module named 'tensorflow.keras.layers.core'
+# from tensorflow.keras.layers.core import Dense, Lambda, Activation
+from tensorflow.keras.layers import Embedding, Input, Dense, Reshape, Flatten, Dropout
+from tensorflow.keras.layers import concatenate   # In Keras 1 there was merge
+# ImportError: cannot import name 'maxnorm' from 'tensorflow.keras.constraints'
+# from tensorflow.keras.constraints import maxnorm
+from tensorflow.keras.optimizers import Adagrad, Adam, SGD, RMSprop
+from tensorflow.keras.losses import BinaryCrossentropy
 from evaluate import evaluate_model
 from Dataset import Dataset
 from time import time
 import sys
 import argparse
 import multiprocessing as mp
+
 
 #################### Arguments ####################
 def parse_args():
@@ -33,8 +44,8 @@ def parse_args():
                         help='Input data path.')
     parser.add_argument('--dataset', nargs='?', default='ml-1m',
                         help='Choose a dataset.')
-    parser.add_argument('--epochs', type=int, default=100,
-                        help='Number of epochs.')
+    parser.add_argument('--epochs', type=int, default=20,
+                        help='Number of epochs.')     # changed default from 100
     parser.add_argument('--batch_size', type=int, default=256,
                         help='Batch size.')
     parser.add_argument('--layers', nargs='?', default='[64,32,16,8]',
@@ -54,38 +65,38 @@ def parse_args():
     return parser.parse_args()
 
 def init_normal(shape, name=None):
-    return initializations.normal(shape, scale=0.01, name=name)
+    return initializers.normal(shape, scale=0.01, name=name)
 
 def get_model(num_users, num_items, layers = [20,10], reg_layers=[0,0]):
     assert len(layers) == len(reg_layers)
-    num_layer = len(layers) #Number of layers in the MLP
+    num_layer = len(layers)  # Number of layers in the MLP
     # Input variables
     user_input = Input(shape=(1,), dtype='int32', name = 'user_input')
     item_input = Input(shape=(1,), dtype='int32', name = 'item_input')
 
-    MLP_Embedding_User = Embedding(input_dim = num_users, output_dim = layers[0]/2, name = 'user_embedding',
-                                  init = init_normal, W_regularizer = l2(reg_layers[0]), input_length=1)
-    MLP_Embedding_Item = Embedding(input_dim = num_items, output_dim = layers[0]/2, name = 'item_embedding',
-                                  init = init_normal, W_regularizer = l2(reg_layers[0]), input_length=1)   
+    MLP_Embedding_User = Embedding(input_dim = num_users, output_dim = round(layers[0]/2), name = 'user_embedding',
+                                  embeddings_initializer='uniform', embeddings_regularizer = L2(l2=reg_layers[0]), input_length=1)
+    MLP_Embedding_Item = Embedding(input_dim = num_items, output_dim = round(layers[0]/2), name = 'item_embedding',
+                                  embeddings_initializer='uniform', embeddings_regularizer = L2(l2=reg_layers[0]), input_length=1)
     
     # Crucial to flatten an embedding vector!
     user_latent = Flatten()(MLP_Embedding_User(user_input))
     item_latent = Flatten()(MLP_Embedding_Item(item_input))
-    
+
     # The 0-th layer is the concatenation of embedding layers
-    vector = merge([user_latent, item_latent], mode = 'concat')
-    
+    vector = concatenate([user_latent, item_latent])
+
     # MLP layers
-    for idx in xrange(1, num_layer):
-        layer = Dense(layers[idx], W_regularizer= l2(reg_layers[idx]), activation='relu', name = 'layer%d' %idx)
+    for idx in range(1, num_layer):
+        layer = Dense(layers[idx], kernel_regularizer= L2(l2=reg_layers[idx]), activation='relu', name = 'layer%d' %idx)
         vector = layer(vector)
-        
+
     # Final prediction layer
-    prediction = Dense(1, activation='sigmoid', init='lecun_uniform', name = 'prediction')(vector)
-    
-    model = Model(input=[user_input, item_input], 
-                  output=prediction)
-    
+    prediction = Dense(1, activation='sigmoid', kernel_initializer=initializers.LecunUniform(), name = 'prediction')(vector)
+
+    model = Model(inputs=[user_input, item_input],
+                  outputs=prediction)
+
     return model
 
 def get_train_instances(train, num_negatives):
@@ -97,9 +108,9 @@ def get_train_instances(train, num_negatives):
         item_input.append(i)
         labels.append(1)
         # negative instances
-        for t in xrange(num_negatives):
+        for t in range(num_negatives):
             j = np.random.randint(num_items)
-            while train.has_key((u, j)):
+            while (u, j) in train.keys():     # while train.has_key((u, j)):
                 j = np.random.randint(num_items)
             user_input.append(u)
             item_input.append(j)
@@ -129,44 +140,44 @@ if __name__ == '__main__':
     dataset = Dataset(args.path + args.dataset)
     train, testRatings, testNegatives = dataset.trainMatrix, dataset.testRatings, dataset.testNegatives
     num_users, num_items = train.shape
-    print("Load data done [%.1f s]. #user=%d, #item=%d, #train=%d, #test=%d" 
+    print("Load data done [%.1f s]. #user=%d, #item=%d, #train=%d, #test=%d"
           %(time()-t1, num_users, num_items, train.nnz, len(testRatings)))
     
     # Build model
     model = get_model(num_users, num_items, layers, reg_layers)
-    if learner.lower() == "adagrad": 
-        model.compile(optimizer=Adagrad(lr=learning_rate), loss='binary_crossentropy')
+    if learner.lower() == "adagrad":
+        model.compile(optimizer=Adagrad(learning_rate=learning_rate), loss=BinaryCrossentropy(from_logits=True))
     elif learner.lower() == "rmsprop":
-        model.compile(optimizer=RMSprop(lr=learning_rate), loss='binary_crossentropy')
+        model.compile(optimizer=RMSprop(learning_rate=learning_rate), loss=BinaryCrossentropy(from_logits=True))
     elif learner.lower() == "adam":
-        model.compile(optimizer=Adam(lr=learning_rate), loss='binary_crossentropy')
+        model.compile(optimizer=Adam(learning_rate=learning_rate), loss=BinaryCrossentropy(from_logits=True))
     else:
-        model.compile(optimizer=SGD(lr=learning_rate), loss='binary_crossentropy')    
-    
+        model.compile(optimizer=SGD(learning_rate=learning_rate), loss=BinaryCrossentropy(from_logits=True))
+
     # Check Init performance
     t1 = time()
     (hits, ndcgs) = evaluate_model(model, testRatings, testNegatives, topK, evaluation_threads)
     hr, ndcg = np.array(hits).mean(), np.array(ndcgs).mean()
     print('Init: HR = %.4f, NDCG = %.4f [%.1f]' %(hr, ndcg, time()-t1))
-    
+
     # Train model
     best_hr, best_ndcg, best_iter = hr, ndcg, -1
-    for epoch in xrange(epochs):
+    for epoch in range(epochs):
         t1 = time()
         # Generate training instances
         user_input, item_input, labels = get_train_instances(train, num_negatives)
-    
-        # Training        
+
+        # Training
         hist = model.fit([np.array(user_input), np.array(item_input)], #input
-                         np.array(labels), # labels 
-                         batch_size=batch_size, nb_epoch=1, verbose=0, shuffle=True)
+                         np.array(labels), # labels
+                         batch_size=batch_size, epochs=1, verbose=0, shuffle=True)
         t2 = time()
 
         # Evaluation
         if epoch %verbose == 0:
             (hits, ndcgs) = evaluate_model(model, testRatings, testNegatives, topK, evaluation_threads)
             hr, ndcg, loss = np.array(hits).mean(), np.array(ndcgs).mean(), hist.history['loss'][0]
-            print('Iteration %d [%.1f s]: HR = %.4f, NDCG = %.4f, loss = %.4f [%.1f s]' 
+            print('Iteration %d [%.1f s]: HR = %.4f, NDCG = %.4f, loss = %.4f [%.1f s]'
                   % (epoch,  t2-t1, hr, ndcg, loss, time()-t2))
             if hr > best_hr:
                 best_hr, best_ndcg, best_iter = hr, ndcg, epoch
